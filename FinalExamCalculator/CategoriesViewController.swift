9eba1e9b903e2d8aa673cb42a2a80c039785e1cf
AustinDev16 <austin.blaser@gmail.com>
2016-09-22 16:14:20 -0600

    Fixed table cell to display category weight.

--- FinalExamCalculator/CategoriesViewController.swift
+++ FinalExamCalculator/CategoriesViewController.swift
@@ -12,8 +12,111 @@ class customCell: UITableViewCell {
     
 }
 
-class CategoriesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
-    // MARK: UIElements
+class CategoriesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
+    // MARK: - Picker Delegate methods
+    var pickerValues: [Int] {
+        var pickerArray: [Int] = []
+        for number in (1...100).reverse() {
+            pickerArray.append(number)
+        }
+        return pickerArray
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return FinalExamController.sharedController.categories.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerValues.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(pickerValues[row])
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let categories = FinalExamController.sharedController.categories
+        let components = FinalExamController.sharedController.categories.count - 1
+        
+        if component < components {
+            // move weight next to it ( + component)
+            let weightToMatch = Int(100 * (Double(categories[component].weight) + Double(categories[component + 1].weight)))
+            print("begin picker")
+            print("Weight to match \(weightToMatch)")
+            
+            let newWeight = pickerValues[row]
+            let newWeightForNextComponent = weightToMatch - newWeight
+            if newWeightForNextComponent < 0 {
+                
+            } else {
+            print("new weight: \(newWeight)")
+            print("next weight: \(newWeightForNextComponent)")
+            
+            // Update weight on object
+            let currentCategory = categories[component]
+            currentCategory.weight = Double(newWeight)/100.0
+            print(currentCategory.weight)
+            
+            let nextCategory = categories[component + 1]
+            nextCategory.weight = Double(newWeightForNextComponent)/100.0
+            print(nextCategory.weight)
+            
+            FinalExamController.sharedController.saveToPersistedStorage()
+            }
+            
+            updatePickerWithWeights()
+            
+        } else {
+            // adjust weight next to it ( - component)
+            // move weight next to it ( + component)
+            let weightToMatch = Int(100 * (Double(categories[component].weight) + Double(categories[component - 1].weight)))
+            print("begin picker")
+            print("Weight to match \(weightToMatch)")
+            
+            let newWeight = pickerValues[row]
+            let newWeightForNextComponent = weightToMatch - newWeight
+            if newWeightForNextComponent < 0 {
+                
+            } else {
+                print("new weight: \(newWeight)")
+                print("next weight: \(newWeightForNextComponent)")
+                
+                // Update weight on object
+                let currentCategory = categories[component]
+                currentCategory.weight = Double(newWeight)/100.0
+                print(currentCategory.weight)
+                
+                let nextCategory = categories[component - 1]
+                nextCategory.weight = Double(newWeightForNextComponent)/100.0
+                print(nextCategory.weight)
+                
+                FinalExamController.sharedController.saveToPersistedStorage()
+            }
+            
+            updatePickerWithWeights()
+        }
+        
+    }
+    
+    func updatePickerWithWeights(){
+        var component = 0
+        for category in FinalExamController.sharedController.categories {
+            print(category.weight)
+            let weight = Double(category.weight) * 100.0
+            if let index = pickerValues.indexOf(Int(weight)) {
+                
+                self.weightPicker.selectRow(index, inComponent: component, animated: true)
+                
+            }
+            
+            component += 1
+            
+            
+        }
+        self.tableView.reloadData()
+    }
+    
+    // MARK: - UIElements
     
     let categoryLabel = UILabel()
     let catSlider = UISlider()
@@ -22,21 +125,28 @@ class CategoriesViewController: UIViewController, UITableViewDelegate, UITableVi
     let addButton = UIButton(type: .System)
     let newCategoryTextField = UITextField()
     let weightLabel = UILabel()
+    let weightPicker = UIPickerView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    
         
         //tableView.frame = CGRectMake(40, 0, 320, 600)
         
         tableView.delegate = self
         tableView.dataSource = self
         
-         tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+//         tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
         
         self.view.backgroundColor = blueColor
         
+        self.weightPicker.delegate = self
+        self.weightPicker.dataSource = self
+        
         setupUIElements()
         setupConstraints()
+        
+        updatePickerWithWeights()
         
   
 
@@ -53,6 +163,7 @@ class CategoriesViewController: UIViewController, UITableViewDelegate, UITableVi
         addButton.translatesAutoresizingMaskIntoConstraints = false
         newCategoryTextField.translatesAutoresizingMaskIntoConstraints = false
         weightLabel.translatesAutoresizingMaskIntoConstraints = false
+        weightPicker.translatesAutoresizingMaskIntoConstraints = false
         
         tableView.backgroundColor = blueColor
         
@@ -76,12 +187,13 @@ class CategoriesViewController: UIViewController, UITableViewDelegate, UITableVi
         
         
         self.view.addSubview(categoryLabel)
-        self.view.addSubview(catSlider)
+        //self.view.addSubview(catSlider)
         self.view.addSubview(tableView)
         self.view.addSubview(doneButton)
         self.view.addSubview(addButton)
         self.view.addSubview(newCategoryTextField)
         self.view.addSubview(weightLabel)
+        self.view.addSubview(weightPicker)
         
     }
 
@@ -121,10 +233,16 @@ class CategoriesViewController: UIViewController, UITableViewDelegate, UITableVi
         let weightTop = NSLayoutConstraint(item: weightLabel, attribute: .Top, relatedBy: .Equal, toItem: tableView, attribute: .Bottom, multiplier: 1, constant: 8)
         self.view.addConstraints([weightCenterX, weightTop])
         
-        // Slider
-        let sliderCenterX = NSLayoutConstraint(item: catSlider, attribute: .CenterX, relatedBy: .Equal, toItem: self.view, attribute: .CenterX, multiplier: 1, constant: 0)
-        let sliderTop = NSLayoutConstraint(item: catSlider, attribute: .Top, relatedBy: .Equal, toItem: weightLabel, attribute: .Top, multiplier: 1, constant: 100)
-        self.view.addConstraints([sliderCenterX, sliderTop])
+        // Weight Picker
+        let weightPickerTop = NSLayoutConstraint(item: weightPicker, attribute: .Top, relatedBy: .Equal, toItem: weightLabel, attribute: .Bottom, multiplier: 1, constant: 20)
+        let weightPickerLeading = NSLayoutConstraint(item: weightPicker, attribute: .Leading, relatedBy: .Equal, toItem: self.view, attribute: .Leading, multiplier: 1, constant: 0)
+        let weightPickerTrailing = NSLayoutConstraint(item: weightPicker, attribute: .Trailing, relatedBy: .Equal, toItem: self.view, attribute: .Trailing, multiplier: 1, constant: 0)
+        self.view.addConstraints([weightPickerTop, weightPickerLeading, weightPickerTrailing])
+        
+//        // Slider
+//        let sliderCenterX = NSLayoutConstraint(item: catSlider, attribute: .CenterX, relatedBy: .Equal, toItem: self.view, attribute: .CenterX, multiplier: 1, constant: 0)
+//        let sliderTop = NSLayoutConstraint(item: catSlider, attribute: .Top, relatedBy: .Equal, toItem: weightLabel, attribute: .Top, multiplier: 1, constant: 100)
+//        self.view.addConstraints([sliderCenterX, sliderTop])
         
 
     }
@@ -137,13 +255,15 @@ class CategoriesViewController: UIViewController, UITableViewDelegate, UITableVi
     
     func addCategoryTapped(){
         
+        newCategoryTextField.resignFirstResponder()
+        
     }
     
     // MARK: - TextFieldDelegate
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         
-        resignFirstResponder()
+        newCategoryTextField.resignFirstResponder()
         
         return true
     }
@@ -156,20 +276,23 @@ class CategoriesViewController: UIViewController, UITableViewDelegate, UITableVi
         return FinalExamController.sharedController.categories.count
     }
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("cell")
+        var cell: UITableViewCell! = tableView.dequeueReusableCellWithIdentifier("cell")
+        if cell == nil {
+            cell = UITableViewCell(style: .Value1, reuseIdentifier: "cell")
+        }
         
         let category = FinalExamController.sharedController.categories[indexPath.row]
         
-        cell?.accessoryType = .DisclosureIndicator
-        cell?.textLabel?.text = category.name
-        cell?.textLabel?.textColor = .whiteColor()
+        cell.accessoryType = .DisclosureIndicator
+        cell.textLabel?.text = category.name
+        cell.textLabel?.textColor = .whiteColor()
         
-        cell?.detailTextLabel?.text = "40%"
+        cell.detailTextLabel?.text = "\(Int(Double(category.weight) * 100))%"
         
-        cell?.backgroundColor = blueColor
+        cell.backgroundColor = blueColor
         
         
-        return cell ?? UITableViewCell()
+        return cell
     }
 
 }
